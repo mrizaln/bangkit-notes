2f863602f128f3105fcc1e3be5529e78c53e62f6
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2023-04-12 07:05:04 +0700

    Update course M.5

---
+++ M.5. Belajar Pengembangan Aplikasi Android Intermediate/03_localization_and_accessibility/MyProductDetail/app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.dicoding.picodiploma.productdetail
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.dicoding.picodiploma.productdetail.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setupView()
+        setupAction()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
+        binding.settingImageView.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+        }
+    }
+}
