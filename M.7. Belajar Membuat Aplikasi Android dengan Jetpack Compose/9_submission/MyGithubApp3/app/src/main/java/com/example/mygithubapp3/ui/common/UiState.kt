67ca08b33ec4a782de11223aeb39b7e0acd31945
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2023-06-08 16:05:33 +0700

    Update MyGithubApp3 to be submodule instead

--- M.7. Belajar Membuat Aplikasi Android dengan Jetpack Compose/9_submission/MyGithubApp3/app/src/main/java/com/example/mygithubapp3/ui/common/UiState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mygithubapp3.ui.common
-
-sealed class UiState<out T : Any?> {
-    object Loading : UiState<Nothing>()
-    data class Success<out T : Any>(val data: T) : UiState<T>()
-    data class Error(val errorMessage: String) : UiState<Nothing>()
-}
