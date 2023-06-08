1ea04279b5d46593b7b68d54c970f9cf29f1f3c5
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2023-06-08 15:58:46 +0700

    Update MyStoryApp2 to be submodule instead

--- M.5. Belajar Pengembangan Aplikasi Android Intermediate/11_submission_2/MyStoryApp2/app/src/main/java/com/example/mystoryapp2/model/data/local/pref/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mystoryapp2.model.data.local.pref
-
-data class User(
-    val name: String,
-    val email: String,
-    val userId: String,
-    val token: String,
-)
--- M.5. Belajar Pengembangan Aplikasi Android Intermediate/11_submission_2/MyStoryApp2/app/src/main/java/com/example/mystoryapp2/model/repository/RequestResult.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mystoryapp2.model.repository
-
-sealed class RequestResult<out R> private constructor() {
-    data class Success<out T>(val data: T) : RequestResult<T>()
-    data class Error(val error: String) : RequestResult<Nothing>()
-    object Loading : RequestResult<Nothing>()
-}
