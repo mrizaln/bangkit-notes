c0172766d973877c8638b0a9923839a7d59d7fd2
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2023-06-08 15:50:47 +0700

    Update MyGithubApp2 to be submodule instead

--- M.4. Belajar Fundamental Aplikasi Android/9_submission_2/MyGithubApp2/app/src/main/java/com/example/mygithubapp2/data/remote/RequestResult.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mygithubapp2.data.remote
-
-sealed class RequestResult<out R> private constructor() {
-    data class Success<out T>(val data: T): RequestResult<T>()
-    data class Error(val error: String): RequestResult<Nothing>()
-    object Loading: RequestResult<Nothing>()
-}
--- M.4. Belajar Fundamental Aplikasi Android/9_submission_2/MyGithubApp2/app/src/main/java/com/example/mygithubapp2/ui/detail/UserDetail.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mygithubapp2.ui.detail
-
-data class UserDetail(
-    val username: String,
-    val fullName: String,
-    val numOfFollowers: Int,
-    val numOfFollowing: Int,
-    val avatarUrl: String,
-)
